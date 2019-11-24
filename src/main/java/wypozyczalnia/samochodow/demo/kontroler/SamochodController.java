c94ad9585e6cb324530331d5c18f57c382383cb5
Damian <damian.bartczuk111@gmail.com>
2019-11-24 13:24:35 +0100

    Dodanie zapisu samochod

--- src/main/java/wypozyczalnia/samochodow/demo/kontroler/SamochodController.java
+++ src/main/java/wypozyczalnia/samochodow/demo/kontroler/SamochodController.java
@@ -3,10 +3,8 @@ package wypozyczalnia.samochodow.demo.kontroler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 import wypozyczalnia.samochodow.demo.model.Samochod;
 import wypozyczalnia.samochodow.demo.repozytorium.SamochodRepository;
 
@@ -14,6 +12,7 @@ import java.util.List;
 
 @RestController
 @Slf4j
+@CrossOrigin
 public class SamochodController {
 
 
@@ -25,14 +24,25 @@ public class SamochodController {
         return "hello";
     }
 
-    @PostMapping("zapis_samochod")
-    public void zapiszSamochod(@RequestBody Samochod samochod){
+    @PostMapping(value = "zapisz_samochod")
+    @Transactional
+    public Samochod zapiszSamochod(@RequestBody Samochod samochod) {
         log.info("zapis samochodu");
-        this.samochodRepository.save(samochod);
+
+        Samochod s = Samochod.builder()
+                .marka(samochod.getMarka())
+                .model(samochod.getModel())
+                .opis(samochod.getOpis())
+                .cenaZaDobe(samochod.getCenaZaDobe())
+                .logo(samochod.getLogo())
+                .czyWypozyczony(samochod.isCzyWypozyczony())
+                .build();
+        log.info(s.toString());
+        return this.samochodRepository.save(s);
     }
 
-    @GetMapping("pobierz_samochody")
-    public List<Samochod> pobierzSamochody(){
+    @GetMapping( value = "pobierz_samochody")
+    public List<Samochod> pobierzSamochody() {
         log.info("Trafiles po odczyt samochodow");
         return this.samochodRepository.findAll();
     }
