e02db64999347c659540ea5e3c9bc872deeed163
Damian <damian.bartczuk111@gmail.com>
2019-11-29 23:33:37 +0100

    Dodanie modelu uzytkownik

---
+++ src/main/java/wypozyczalnia/samochodow/demo/model/Uzytkownik.java
@@ -0,0 +1,29 @@
+package wypozyczalnia.samochodow.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+public class Uzytkownik {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    @Transient
+    private String passwordConfirm;
+
+    @ManyToMany
+    private Set<Rola> roles;
+
+    public Uzytkownik(){}
+}
