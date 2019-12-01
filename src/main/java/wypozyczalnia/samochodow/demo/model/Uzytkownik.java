a40d5e3588faa8108739449c2b50ef4abb5cd47a
Damian <damian.bartczuk111@gmail.com>
2019-12-01 17:39:34 +0100

    Poprawka modelu uzytkownik

--- src/main/java/wypozyczalnia/samochodow/demo/model/Uzytkownik.java
+++ src/main/java/wypozyczalnia/samochodow/demo/model/Uzytkownik.java
@@ -1,5 +1,6 @@
 package wypozyczalnia.samochodow.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,22 +8,35 @@ import javax.persistence.*;
 import java.util.Set;
 
 @Entity
-@Table(name = "user")
+@Table(name = "uzytkownik")
 @Getter
 @Setter
 public class Uzytkownik {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "id_uzytkownik")
+    private int idUzytkownik;
 
+    @Column( name = "imie")
+    private String imie;
+
+    @Column(name = "nazwisko")
+    private String nazwisko;
+
+    @Column(name = "nazwa_uzytkownika")
     private String username;
 
+    @Column(name = "haslo")
     private String password;
 
     @Transient
     private String passwordConfirm;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "uzytkownik_rola", joinColumns = {
+            @JoinColumn(name = "id_uzytkownik") },
+            inverseJoinColumns = { @JoinColumn(name = "id_rola")})
+    @JsonIgnore
     private Set<Rola> roles;
 
     public Uzytkownik(){}
