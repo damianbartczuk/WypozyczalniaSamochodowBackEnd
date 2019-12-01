fe26ee3a2faa518a85eb70d405d7c7efc3fb4f09
Damian <damian.bartczuk111@gmail.com>
2019-12-01 17:43:54 +0100

    Metoda do pobrania wszystkich ról

--- src/main/java/wypozyczalnia/samochodow/demo/repozytorium/RolaRepository.java
+++ src/main/java/wypozyczalnia/samochodow/demo/repozytorium/RolaRepository.java
@@ -1,9 +1,15 @@
 package wypozyczalnia.samochodow.demo.repozytorium;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import wypozyczalnia.samochodow.demo.model.Rola;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface RolaRepository extends JpaRepository<Rola, Integer> {
+public interface RolaRepository extends JpaRepository<Rola, Long> {
+
+        @Query(value = "select r from  Rola r")
+    List<Rola> pobierzKonkretneRole();
 }
