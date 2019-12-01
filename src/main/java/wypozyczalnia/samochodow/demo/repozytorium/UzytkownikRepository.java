50cc5b13877e974f0464234753bf6e4902e7928b
Damian <damian.bartczuk111@gmail.com>
2019-12-01 17:39:03 +0100

    Dodanie metody do pobierania uzytkownika poprzez username

--- src/main/java/wypozyczalnia/samochodow/demo/repozytorium/UzytkownikRepository.java
+++ src/main/java/wypozyczalnia/samochodow/demo/repozytorium/UzytkownikRepository.java
@@ -1,9 +1,13 @@
 package wypozyczalnia.samochodow.demo.repozytorium;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import wypozyczalnia.samochodow.demo.model.Rola;
+import wypozyczalnia.samochodow.demo.model.Uzytkownik;
 
 @Repository
-public interface UzytkownikRepository extends JpaRepository<Rola, Integer> {
+public interface UzytkownikRepository extends JpaRepository<Uzytkownik, Integer> {
+
+    @Query(value = "from Uzytkownik u where u.username = :username")
+    Uzytkownik findByUsername(String username);
 }
