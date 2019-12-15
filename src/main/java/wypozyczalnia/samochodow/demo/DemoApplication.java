afaaa1b0c62c976a3e5aac07a2f1db01d73562dd
Damian <damian.bartczuk111@gmail.com>
2019-12-15 01:07:26 +0100

    Konfiguraca liquibase

--- src/main/java/wypozyczalnia/samochodow/demo/DemoApplication.java
+++ src/main/java/wypozyczalnia/samochodow/demo/DemoApplication.java
@@ -1,17 +1,23 @@
 package wypozyczalnia.samochodow.demo;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import wypozyczalnia.samochodow.demo.kontroler.SamochodController;
-import wypozyczalnia.samochodow.demo.model.Samochod;
-import wypozyczalnia.samochodow.demo.repozytorium.SamochodRepository;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public SpringLiquibase liquibase() {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+//		liquibase.setDataSource();
+		return liquibase;
 	}
 
 }
