c20d03703baebc42a03ea5bf1efdebe5f74bcbed
Damian <damian.bartczuk111@gmail.com>
2020-01-12 00:20:40 +0100

    Dostosowanie Userów do paginacji + dodanie możliwośći pobrania użytkownika zalogowanego + toString dla Usera

--- src/main/java/wypozyczalnia/samochodow/demo/kontroler/UserController.java
+++ src/main/java/wypozyczalnia/samochodow/demo/kontroler/UserController.java
@@ -3,32 +3,44 @@ package wypozyczalnia.samochodow.demo.kontroler;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import wypozyczalnia.samochodow.demo.jwtauth.JwtTokenUtil;
 import wypozyczalnia.samochodow.demo.model.User;
 import wypozyczalnia.samochodow.demo.serwisy.UserService;
 
-import java.util.List;
-
-@CrossOrigin(origins = "http://localgost:4200", exposedHeaders = "Authorization")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@Slf4j
-@AllArgsConstructor
 @Api(tags = "User API")
-@RequestMapping(value = "api/user")
 public class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
     private UserService userService;
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    public UserController(UserService userService, JwtTokenUtil jwtTokenUtil){
+        this.userService = userService;
+        this.jwtTokenUtil = jwtTokenUtil;
+    }
+
+    @GetMapping(value = "pobierz_uzytkownika_za_pomoca_tokenu")
+    public ResponseEntity<User> pobierzUzytkownikaZTokeny(@RequestParam(value = "token") String token){
+        String username = this.jwtTokenUtil.getUsernameFromToken(token);
+        User user = userService.findByUsername(username);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
 
     @ApiOperation(value = "Pobiera informacje na temat użytkowników")
     @GetMapping( value = "pobierz_uzytkownikow")
-    public ResponseEntity<List<User>> pobierzUzytkownikow() {
+    public ResponseEntity<Page<User>> pobierzUzytkownikow(Pageable pageable) {
         log.info("Pobranie uzytkownikow");
-        return new ResponseEntity<>(this.userService.odczytUzytkownikow(), HttpStatus.OK);
+        return new ResponseEntity<>(this.userService.odczytUzytkownikow(pageable), HttpStatus.OK);
     }
 
     @ApiOperation(value = "Zapisuje użytkownika, kodowanie hasła jest na poziomie kontrolera")
--- src/main/java/wypozyczalnia/samochodow/demo/repozytorium/CarRepository.java
+++ src/main/java/wypozyczalnia/samochodow/demo/repozytorium/CarRepository.java
@@ -1,9 +1,10 @@
 package wypozyczalnia.samochodow.demo.repozytorium;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import wypozyczalnia.samochodow.demo.model.Car;
 
 @Repository
-public interface CarRepository extends PagingAndSortingRepository<Car, Integer> {
+public interface CarRepository extends PagingAndSortingRepository<Car, Integer>, JpaRepository<Car, Integer> {
 }
