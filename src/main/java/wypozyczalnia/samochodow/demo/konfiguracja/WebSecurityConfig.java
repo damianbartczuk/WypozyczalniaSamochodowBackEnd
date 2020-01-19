709b71e35509204acd5faa225321ef7a3f72c2bf
Damian <damian.bartczuk111@gmail.com>
2020-01-19 17:00:58 +0100

    Konfiguracja dostępności do endpointów

--- src/main/java/wypozyczalnia/samochodow/demo/konfiguracja/WebSecurityConfig.java
+++ src/main/java/wypozyczalnia/samochodow/demo/konfiguracja/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf()
                 .disable()
                 .authorizeRequests().antMatchers("/authenticate").permitAll()
-                .antMatchers("/pobierz_uzytkownikow", "/pobierz_samochody", "/save_rental", "pobierz_uzytkownika_za_pomoca_tokenu")
+                .antMatchers("/pobierz_uzytkownikow", "/save_rental", "pobierz_uzytkownika_za_pomoca_tokenu", "/pobierz_samochody")
                 .authenticated()
                 .and()
 //                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
@@ -69,3 +69,63 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
+
+//
+///**
+// * This class defines the Spring Security configuration for the web application.
+// */
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Autowired
+//    private DataSource dataSource;
+//
+//    /**
+//     * Defines what queries should be used for verifying users and loading their permissions, in addition to the data source and the method of processing passwords.
+//     * @param auth the AuthenticationManagerBuilder to configure
+//     */
+//    @Override
+//    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+//        auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(new PasswordEncoder())
+//                .usersByUsernameQuery("SELECT username,password, 1 as enabled FROM \"USER\" WHERE username=?")
+//                .authoritiesByUsernameQuery("SELECT username, permissions FROM \"USER\",\"ROLE\" WHERE \"USER\".\"ROLE\" = \"ROLE\".id AND username=?");
+//    }
+//
+//    /**
+//     * Defines the HttpSecurity configuration to enforce authentication for endpoints and restrict access to certain endpoints based on permissions.
+//     * Established rules
+//     * <p>
+//     *     <ul>
+//     *         <li>Each user can get or edit his user object</li>
+//     *         <li>User data is accessible only to ADMINs</li>
+//     *         <li>Insert/Delete/Edit endpoints are accessible only to ADMIN users</li>
+//     *         <li>Users with REPORT_ONLY roles can only access reports endpoints</li>
+//     *         <li>Users with VIEW roles can access all data retrieval endpoints except those under /user</li>
+//     *     </ul>
+//     * </p>
+//     * @param http The HttpSecurity object to configure
+//     */
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.cors()
+//                .and().csrf().disable()
+//                .exceptionHandling()
+//                .authenticationEntryPoint(new RestAuthenticationEntryPoint())
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/user/getCurrent/**").authenticated()
+//                .antMatchers("/user/editCurrent/**").authenticated()
+//                .regexMatchers("^.*user.*$").hasAuthority("ADMIN")
+//                .regexMatchers("^.*insert.*$").hasAuthority("ADMIN")
+//                .regexMatchers("^.*delete.*$").hasAuthority("ADMIN")
+//                .regexMatchers("^.*edit.*$").hasAuthority("ADMIN")
+//                .regexMatchers("^((?!reports).)*$").hasAnyAuthority("VIEW","ADMIN")
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .successHandler(new MySavedRequestAwareAuthenticationSuccessHandler())
+//                .failureHandler(new SimpleUrlAuthenticationFailureHandler())
+//                .and()
+//                .logout();
+//    }
+//}
